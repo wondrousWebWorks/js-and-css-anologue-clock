fe61739be8571571255c1606a63ec8f616884d2f
Johann Alberts <alberts.johann@gmail.com>
2020-12-07 12:10:13 +0000

    Add logic for minute and hour hands

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,18 @@
 const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 const setDate = () => {
   const now = new Date();
   const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hour = now.getHours();
   const secondDegrees = ((seconds / 60) * 360 + 90);
+  const minuteDegrees = ((minutes / 60) * 360 + 90);
+  const hourDegrees = ((hour / 12) * 360 + 90);
   secondHand.style.transform = `rotate(${secondDegrees}deg)`;
-
-  console.log(seconds);
+  minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
