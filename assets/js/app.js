4652d2b0c63ed1df88394dbd63dfd9bd9af7963a
Johann Alberts <alberts.johann@gmail.com>
2020-12-07 11:47:17 +0000

    Add logic to animate seconds hand

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+const secondHand = document.querySelector('.second-hand');
+
+const setDate = () => {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondDegrees = ((seconds / 60) * 360 + 90);
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  console.log(seconds);
+}
+
+setInterval(setDate, 1000);
